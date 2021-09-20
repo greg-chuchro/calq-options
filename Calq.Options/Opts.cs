b0f3500c863af85c05e6d738c09db7bb318824a2
greg-chuchro <gchuchro@acm.org>
2021-09-20 14:53:46 +0900

    #16 allow to skip unknown options

--- Calq.Options/Opts.cs
+++ Calq.Options/Opts.cs
@@ -8,6 +8,11 @@ namespace Ghbvft6.Calq.Options {
 
         public static int Load<T>(T instance, string[] args, int startIndex = 0) where T : notnull {
             var reader = new Reader<T>();
+            Load(reader, instance, args, startIndex);
+            return reader.LastIndex;
+        }
+
+        private static void Load<T>(Reader<T> reader, T instance, string[] args, int startIndex) where T : notnull {
             foreach (var (option, value) in reader.Read(args, startIndex)) {
                 var valueObj = Reflection.ParseValue(Reflection.GetFieldOrPropertyType(instance.GetType(), option), value, option);
                 try {
@@ -17,7 +22,30 @@ namespace Ghbvft6.Calq.Options {
                     throw new Exception($"option and value type mismatch: {option}={value} ({option} is {type.Name})", ex);
                 }
             }
-            return reader.LastIndex;
+        }
+
+        public static void LoadSkipUnknown<T>(T instance) where T : notnull {
+            LoadSkipUnknown(instance, Environment.GetCommandLineArgs(), 1);
+        }
+
+        public static void LoadSkipUnknown<T>(T instance, string[] args, int startIndex = 0) where T : notnull {
+            var reader = new Reader<T>();
+            while (reader.LastIndex < args.Length) {
+                try {
+                    Load(reader, instance, args, startIndex);
+                    if (reader.LastIndex == startIndex) {
+                        ++startIndex;
+                    } else {
+                        startIndex = reader.LastIndex;
+                    }
+                } catch (Exception ex) {
+                    if (ex.Message.StartsWith("option doesn't exist")) {
+                        startIndex = reader.LastIndex + 1;
+                    } else {
+                        throw;
+                    }
+                }
+            }
         }
     }
 }
